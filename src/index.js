7f7dd68478bf8be0dbd28c6ca4b85a01a1306fbc
Rene Ramirez <rdejramirez@gmail.com>
2022-07-12 00:28:57 -0500

    feat: conexion database and docker compose image psql db

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import express from "express";
+import { sequelize } from "./conexion";
 
+async function main() {
+  const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 5000;
+  const app = express();
 
-const app = express();
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.static(__dirname + "./../public"));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname + "./../public"));
+  await sequelize.sync({ force: false });
 
-app.listen(PORT, () => {
+  app.listen(PORT, () => {
     console.log("Server on port", PORT);
-});
+  });
+}
+
+main();
