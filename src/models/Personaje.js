96571778bd841f1a44d7819212ec31c23427e107
Rene Ramirez <rdejramirez@gmail.com>
2022-07-17 04:38:51 -0500

    feat: Personaje validations added

--- src/models/Personaje.js
+++ src/models/Personaje.js
@@ -10,15 +10,40 @@ Personaje.init(
     },
     nombre: {
       type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: {
+          args: [5, 25],
+          msg: "Must be bewteen 5 and 25 characters",
+        },
+      },
     },
     edad: {
       type: DataTypes.INTEGER,
+      validate: {
+        isNumeric: {
+          args: true,
+          msg: "Must be a valid age",
+        },
+      },
     },
     peso: {
       type: DataTypes.FLOAT,
+      validate: {
+        isNumeric: {
+          args: true,
+          msg: "Must be a valid weight",
+        },
+      },
     },
     historia: {
       type: DataTypes.TEXT,
+      validate: {
+        len: {
+          args: [35, 255],
+          msg: "Must be between 35 and 255 characters",
+        },
+      },
     },
   },
   {
@@ -29,4 +54,3 @@ Personaje.init(
 );
 
 module.exports = Personaje;
-
